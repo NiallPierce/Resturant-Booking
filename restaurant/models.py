3f37d01b32d5cb8891033a07b43f3e1a891714f2
Niall Pierce <niallpierce92@hotmail.com>
2024-12-30 09:42:48 +0000

    add class for menuitem

--- restaurant/models.py
+++ restaurant/models.py
@@ -10,7 +10,7 @@ class Restaurant(models.Model):
 
     def __str__(self):
         return self.name  # String representation of the restaurant
-        
+
 class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)  # Link to Django's built-in User model
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
@@ -21,3 +21,12 @@ class Booking(models.Model):
 
     def __str__(self):
         return f"Booking for {self.user.username} on {self.date} at {self.time}"
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - ${self.price}"
